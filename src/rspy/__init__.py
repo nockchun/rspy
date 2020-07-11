7991a87803df1801d3d8899acdcc59071055e4b4
nockchun <nockchun@gmail.com>
2020-07-12 00:28:12 +0900

    fix import error

--- src/rspy/__init__.py
+++ src/rspy/__init__.py
@@ -2,6 +2,7 @@
 from pkg_resources import get_distribution, DistributionNotFound
 from .util import *
 from .preprocess import *
+from .database import *
 
 try:
     # Change here if project is renamed and does not equal the package name
