81b9eb51f686d7b7ea567b455cd061ed89dcb933
nockchun <nockchun@gmail.com>
2020-08-16 01:15:25 +0900

    add rolling function

--- src/rspy/preprocess/sequence.py
+++ src/rspy/preprocess/sequence.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-def genSequence(data, windowSize, labelSize=1):
-    seqData = data[:-(windowSize+labelSize-1)].reshape(-1, 1)
-    seqLabel = data[windowSize:-(labelSize-1) if labelSize > 1 else None].reshape(-1, 1)
-    for idx in range(1, windowSize):
-        seqData = np.concatenate([seqData, data[idx:-(windowSize+labelSize-idx-1)].reshape(-1, 1)], axis=1)
-    for idx in range(1, labelSize):
-        lastIdx = labelSize-idx-1
-        seqLabel = np.concatenate([seqLabel, data[windowSize+idx:-lastIdx if lastIdx > 0 else None].reshape(-1, 1)], axis=1)
-    return seqData.reshape(-1, windowSize, 1), seqLabel.reshape(-1, labelSize, 1)
+def getRolling(data, windowSize):
+    sliceSize = len(data) - windowSize + 1
+    if sliceSize <= 0:
+        raise ValueError("Rolling not possible, windowSize is larger then data length.")
+        
+    shape = (sliceSize, windowSize) + data.shape[1:]
+    strides = (data.strides[0],) + data.strides
+
+    return as_strided(data, shape=shape, strides=strides)
+
